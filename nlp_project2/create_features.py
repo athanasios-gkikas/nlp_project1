d76afc82bc7b7c442d53d80c6309700e77db4422
vasilikikou <vasilikvasilikikou@gmail.com>
2019-03-13 01:26:06 +0200

    Added preprocessing

--- nlp_project2/create_features.py
+++ nlp_project2/create_features.py
@@ -3,7 +3,6 @@ from nltk.tokenize import word_tokenize
 from string import punctuation 
 from nltk.corpus import stopwords
 
-
 def load_tsv(filepath):
 	tweets = {}
 
@@ -19,7 +18,7 @@ def load_tsv(filepath):
 	return tweets
 
 def process_tweets(tweets):
-	tweet_stopwords = set(stopwords.words('english') + list(punctuation) + ['AT_USER','URL'])
+	tweet_stopwords = set(stopwords.words('english') + list(punctuation) + ["..."] + ['AT_USER','URL'])
 	new_tweets = {}
 
 	for tweet in tweets:
@@ -27,6 +26,8 @@ def process_tweets(tweets):
 		text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', text) # remove URLs
 		text = re.sub('@[^\s]+', 'AT_USER', text) # remove usernames
 		text = re.sub(r'#([^\s]+)', r'\1', text) # remove the # in #hashtag
+		text = re.sub('([.]+)', '.', text) #remove multiple dots
+		text = re.sub(r"(?:['\-_]+[a-z])", ' ', text) #remove single characters with ' or -
 		text = word_tokenize(text) # remove repeated characters (helloooooooo into hello)
 		text = [word for word in text if word not in tweet_stopwords]
 
@@ -42,13 +43,23 @@ def main():
 	dev_tweets = load_tsv("./dataset/dev_set.tsv")
 	test_tweets = load_tsv("./dataset/test_set.tsv")
 
-	pr_train_tweets = process_tweets(train_tweets)
+	# pr_train_tweets = process_tweets(train_tweets)
 
-	print(list(train_tweets.keys())[0])
-	print(train_tweets[list(train_tweets.keys())[0]])
+	# print(list(train_tweets.keys())[0])
+	# print(train_tweets[list(train_tweets.keys())[0]])
 
-	print(list(pr_train_tweets.keys())[0])
-	print(pr_train_tweets[list(pr_train_tweets.keys())[0]])
+	# print(list(pr_train_tweets.keys())[0])
+	# print(pr_train_tweets[list(pr_train_tweets.keys())[0]])
+
+	test_tweet={}
+	test_tweet["1"] = ["negative", "vaso @petros I am vaso :D friend's #love cats ... / ? helloooooooo"]
+
+	new_tweet = process_tweets(test_tweet)
+	print(list(test_tweet.keys())[0])
+	print(test_tweet[list(test_tweet.keys())[0]])
+
+	print(list(new_tweet.keys())[0])
+	print(new_tweet[list(new_tweet.keys())[0]])
 
 
 if __name__ == "__main__":
