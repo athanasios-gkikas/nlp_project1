ca70d4603cb19e8383fddeaf89e1d763a89fe0bb
unknown <iordaniseu@hotmail.com>
2019-03-25 18:07:26 +0200

    refactoring

--- project_2/classifiers.py
+++ project_2/classifiers.py
@@ -1,25 +1,24 @@
 import re
-from nltk.tokenize import word_tokenize
-from string import punctuation
-from nltk.corpus import stopwords
-from sklearn import preprocessing
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+
+from sklearn import preprocessing
 from sklearn.model_selection import learning_curve
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.dummy import DummyClassifier
 from sklearn.svm import LinearSVC, SVC, NuSVR
-from sklearn.metrics import f1_score
-from sklearn.metrics import precision_recall_curve
-from sklearn.metrics import average_precision_score
+from sklearn.metrics import precision_recall_curve, average_precision_score, f1_score
 from sklearn.preprocessing import label_binarize
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.decomposition import TruncatedSVD
 from time import time
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
 
 def load_tsv(filepath):
 	tweets = {}
@@ -31,133 +30,121 @@ def load_tsv(filepath):
 
 	return tweets
 
-
-def process_tweets(tweets):
-	tweet_stopwords = set(stopwords.words('english') + list(punctuation) + ["..."] + ['AT_USER', 'URL'])
-	new_tweets = {}
-
-	for tweet in tweets:
-		text = tweets[tweet][1].lower()  # convert text to lower-case
-		text = find_emoticons_in_tweets(text)  # convert emoticons to text
-		text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', text)  # remove URLs
-		text = re.sub('@[^\s]+', 'AT_USER', text)  # remove usernames
-		text = re.sub(r'#([^\s]+)', r'\1', text)  # remove the # in #hashtag
-		text = re.sub('([.]+)', '.', text)  # remove multiple dots
-		text = re.sub(r"(?:['\-_]+[a-z])", ' ', text)  # remove single characters with ' or -
-		text = word_tokenize(text)
-		text = [word for word in text if word not in tweet_stopwords]
-
-		new_tweets[tweet] = [tweets[tweet][0], " ".join(text)]
-
-	return new_tweets
-
-
-def find_emoticons_in_tweets(tweet):
-	new_tweet = tweet
-	# All caps? Does it matter?
-	repl = {' :)': ' HAPPY_EMOTICON', ' =)': ' HAPPY_EMOTICON', ' :d': ' VERY_HAPPY_EMOTICON', ' :(': ' SAD_EMOTICON',
-	        ' :/': ' MIXED_EMOTICON', ' :p': ' TONGUE_EMOTICON', ' ;)': ' WINK_EMOTICON'}
-	for a, b in repl.items():
-		new_tweet = new_tweet.replace(a, b)
-
-	return new_tweet
-
-
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
-                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 10), scoring='f1_macro'):
-	plt.figure()
-	plt.title(title)
-	if ylim is not None:
-		plt.ylim(*ylim)
-	plt.xlabel("Training examples")
-	plt.ylabel("Score")
-	train_sizes, train_scores, test_scores = learning_curve(
-		estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes,
-		scoring=scoring)
-	train_scores_mean = np.mean(train_scores, axis=1)
-	train_scores_std = np.std(train_scores, axis=1)
-	test_scores_mean = np.mean(test_scores, axis=1)
-	test_scores_std = np.std(test_scores, axis=1)
-	plt.grid()
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 10), scorer='f1_macro'):
 
-	plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
-	                 train_scores_mean + train_scores_std, alpha=0.1,
-	                 color="r")
-	plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
-	                 test_scores_mean + test_scores_std, alpha=0.1, color="b")
-	plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-	         label="Training score")
-	plt.plot(train_sizes, test_scores_mean, 'o-', color="b",
-	         label="Test score")
+    plt.subplot(2, 2, 1)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    plt.xlabel("Training examples")
 
-	plt.legend(loc="best")
-	return plt
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, scoring=scorer)
 
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                        train_scores_mean + train_scores_std, alpha=0.1,
+                        color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                        test_scores_mean + test_scores_std, alpha=0.1, color="b")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+                label="Training F1-macro")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="b",
+                label="Test F1-macro")
+
+    plt.legend(loc="best")
+
+    plt.subplot(2, 2, 2)
+    plt.xlabel("Training examples")
+
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, scoring='accuracy')
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                        train_scores_mean + train_scores_std, alpha=0.1,
+                        color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                        test_scores_mean + test_scores_std, alpha=0.1, color="b")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+                label="Training accuracy")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="b",
+                label="Test accuracy")
+
+    plt.legend(loc="best")
+
+    #plt.show()
 
 def plot_precision_recall_curves(estimator, name, x_train, y_1, x_test, y_2):
-	y_train = label_binarize(y_1, classes=[0, 1, 2])
-	y_test = label_binarize(y_2, classes=[0, 1, 2])
+    y_train = label_binarize(y_1, classes=[0, 1, 2])
+    y_test = label_binarize(y_2, classes=[0, 1, 2])
 
-	labels = ["negative", "neutral", "positive"]
+    labels = ["negative", "neutral", "positive"]
 
-	classifier = OneVsRestClassifier(estimator)
-	y_score = classifier.fit(x_train, y_train).decision_function(x_test)
+    classifier = OneVsRestClassifier(estimator)
+    y_score = classifier.fit(x_train, y_train).predict_proba(x_test)
 
-	# Compute Precision-Recall and plot curve
-	precision = dict()
-	recall = dict()
-	average_precision = dict()
-	n_classes = 3
-	for i in range(n_classes):
-		precision[i], recall[i], _ = precision_recall_curve(y_test[:, i],
-		                                                    y_score[:, i])
-		average_precision[i] = average_precision_score(y_test[:, i], y_score[:, i])
-	# Compute micro-average ROC curve and ROC area
-	precision["micro"], recall["micro"], _ = precision_recall_curve(y_test.ravel(),
-	                                                                y_score.ravel())
-	average_precision["micro"] = average_precision_score(y_test, y_score,
-	                                                     average="micro")
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+    n_classes = 3
 
-	# Plot Precision-Recall curve
+    for i in range(n_classes):
+        precision[i], recall[i], _ = precision_recall_curve(
+            y_test[:, i], y_score[:, i])
 
-	plt.clf()
-	plt.plot(recall["micro"], precision["micro"], color='navy',
-	         label='micro-average Precision-recall curve (area = {0:0.2f})'
-	               ''.format(average_precision["micro"]))
-	plt.xlabel('Recall')
-	plt.ylabel('Precision')
-	plt.ylim([0.0, 1.05])
-	plt.xlim([0.0, 1.0])
-	plt.title('Precision-Recall {0}: AUC={1:0.2f}'.format(name, average_precision['micro']))
-	plt.legend(loc="lower left")
-	plt.show()
+        average_precision[i] = average_precision_score(
+            y_test[:, i], y_score[:, i])
 
-	# Plot Precision-Recall curve for each class
-	plt.clf()
-	plt.plot(recall["micro"], precision["micro"], color='navy',
-	         label='micro-average Precision-recall curve (area = {0:0.2f})'
-	               ''.format(average_precision["micro"]))
-	for i, color in zip(range(3), ['red', 'pink', 'blue']):
-		plt.plot(recall[i], precision[i], color=color,
-		         label='Precision-recall curve of class {0} (area = {1:0.2f})'
-		               ''.format(labels[i], average_precision[i]))
+    precision["micro"], recall["micro"], _ = precision_recall_curve(
+        y_test.ravel(), y_score.ravel())
 
-	plt.xlim([0.0, 1.0])
-	plt.ylim([0.0, 1.05])
-	plt.xlabel('Recall')
-	plt.ylabel('Precision')
-	plt.title('Extension of Precision-Recall curve to multi-class ({0})'.format(name))
-	plt.legend(loc="lower right")
-	plt.show()
-	return
+    average_precision["micro"] = average_precision_score(
+        y_test, y_score, average="micro")
 
-def encodeSet(pSet) :
+    plt.subplot(2, 2, 3)
+
+    plt.plot(recall["micro"], precision["micro"], color='navy',
+        label='micro-average AUC= {0:0.2f}'.format(average_precision['micro']))
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.legend(loc="best")
+    plt.grid()
+
+    plt.subplot(2, 2, 4)
+
+    for i, color in zip(range(3), ['red', 'pink', 'blue']):
+        plt.plot(recall[i], precision[i], color=color,
+                    label='{0} AUC= {1:0.2f}'
+                        ''.format(labels[i], average_precision[i]))
+
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.legend(loc="lower left")
+    plt.grid()
+    plt.show()
+    return
+
+def encodeSet(pSet, pEncoder) :
 
     x = [s[1] for s in list(pSet.values())]
     y = [s[0] for s in list(pSet.values())]
-    le = preprocessing.LabelEncoder()
-    le.fit(y)
-    y = le.transform(y)
+    y = pEncoder.transform(y)
 
     return x, y
 
@@ -186,13 +173,14 @@ def tuneSVC(pTrain) :
     pipeline = Pipeline([
         ('clf',  OneVsRestClassifier(SVC(
             class_weight="balanced",
-            max_iter=1000,
+            max_iter=5000,
             gamma='scale',
             random_state=0))),])
 
     parameters = {
-        'clf__estimator__kernel' : ['linear', 'rbf', 'poly'],
-        'clf__estimator__C' : [0.01, 0.1, 1.0, 10.0, 100.0],}
+        'clf__estimator__kernel' : ['linear', 'rbf', 'poly', 'sigmoid'],
+        'clf__estimator__C' : [0.01, 0.1, 1.0, 10.0],
+        'clf__estimator__coef0' : [3, 4],}
 
     return tuner(pTrain, pipeline, parameters)
 
@@ -216,7 +204,7 @@ def tuneSGD(pTrain) :
 def tuneBase(pTrain) :
 
     pipeline = Pipeline([
-        ('clf',  DummyClassifier(strategy = "most_frequent", random_state=0)),])
+        ('clf',  DummyClassifier(random_state=0)),])
 
     parameters = { 'clf__strategy': ['most_frequent', 'stratified'], }
 
@@ -295,17 +283,34 @@ def approxDim(pData) :
 
     return int(np.exp(arr.sum() / len(arr)))
 
+def plotClassifier(pEstimator, pTrain, pTest) :
+
+    fig = plt.figure(figsize=(8, 7))
+    plt.subplots_adjust(wspace=0.3, hspace=0.3)
+
+    plot_learning_curve(pEstimator,
+        "Learning curves",
+        pTrain[0], pTrain[1], (0.1, 1.01), cv=5,
+        n_jobs=-1)
+
+    plot_precision_recall_curves(pEstimator,
+        "P-R curves",
+        pTrain[0], pTrain[1], pTest[1], pTest[0])
+
+    return
+
 def main():
 
     cwd = os.getcwd()
+
     train_tweets = load_tsv(cwd + "/project_2/dataset/train_set.tsv")
     test_tweets = load_tsv(cwd + "/project_2/dataset/test_set.tsv")
 
-    pr_train_tweets = process_tweets(train_tweets)
-    pr_test_tweets = process_tweets(test_tweets)
+    encoder = preprocessing.LabelEncoder()
+    encoder.fit(["negative", "neutral", "positive"])
 
-    x_train, y_train = encodeSet(pr_train_tweets)
-    x_test, y_test = encodeSet(pr_test_tweets)
+    x_train, y_train = encodeSet(train_tweets, encoder)
+    x_test, y_test = encodeSet(test_tweets, encoder)
 
     f = 2281 #approxDim(y_train)
     vectorizer = TfidfVectorizer(
@@ -320,19 +325,12 @@ def main():
     x_train = vectorizer.transform(x_train)
     x_test = vectorizer.transform(x_test)
 
-    #model = tuneBase([x_train, y_train])
-    model = tuneSVC([x_train, y_train])
+    model = tuneBase([x_train, y_train])
+    #model = tuneSVC([x_train, y_train])
     #model = tuneSGD([x_train, y_train])
     #model = tuneLogistic([x_train, y_train])
 
-    plot_learning_curve(model,
-        "Learning Curves for Logistic Regression",
-        x_train, y_train, (0.1, 1.01), cv=5,
-        n_jobs=-1).show()
-
-    plot_precision_recall_curves(model,
-        "Logistic Regression",
-        x_train, y_train, x_test, y_test)
+    plotClassifier(model, [x_train, y_train], [x_test, y_test])
 
 if __name__ == "__main__":
 	main()
--- project_2/load_dataset.py
+++ project_2/load_dataset.py
@@ -1,9 +1,13 @@
-from collections import Counter
 import os
-from shutil import rmtree
 import numpy
 import random
+import re
+
+from shutil import rmtree
 from collections import Counter
+from nltk.tokenize import word_tokenize
+from string import punctuation
+from nltk.corpus import stopwords
 
 def split_data(tweets, keys):
 	new_tweets = {}
@@ -65,78 +69,107 @@ def merge(first_set, second_set):
 
 	return total
 
+def process_tweets(tweets):
+	tweet_stopwords = set(stopwords.words('english') + list(punctuation) + ["..."] + ['AT_USER', 'URL'])
+	new_tweets = {}
+
+	for tweet in tweets:
+		text = tweets[tweet][1].lower()  # convert text to lower-case
+		text = find_emoticons_in_tweets(text)  # convert emoticons to text
+		text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))', 'URL', text)  # remove URLs
+		text = re.sub('@[^\s]+', 'AT_USER', text)  # remove usernames
+		text = re.sub(r'#([^\s]+)', r'\1', text)  # remove the # in #hashtag
+		text = re.sub('([.]+)', '.', text)  # remove multiple dots
+		text = re.sub(r"(?:['\-_]+[a-z])", ' ', text)  # remove single characters with ' or -
+		text = word_tokenize(text)
+		text = [word for word in text if word not in tweet_stopwords]
+
+		new_tweets[tweet] = [tweets[tweet][0], " ".join(text)]
+
+	return new_tweets
+
+def find_emoticons_in_tweets(tweet):
+	new_tweet = tweet
+	# All caps? Does it matter?
+	repl = {' :)': ' HAPPY_EMOTICON', ' =)': ' HAPPY_EMOTICON', ' :d': ' VERY_HAPPY_EMOTICON', ' :(': ' SAD_EMOTICON',
+	        ' :/': ' MIXED_EMOTICON', ' :p': ' TONGUE_EMOTICON', ' ;)': ' WINK_EMOTICON'}
+	for a, b in repl.items():
+		new_tweet = new_tweet.replace(a, b)
+
+	return new_tweet
 
 def main():
 
-	#load data
-	train_path = "twitter_download/train_set.tsv"
-	dev_path = "twitter_download/dev_set.tsv"
-	test_path = "twitter_download/test_set.tsv"
+    #load data
+    train_path = "twitter_download/train_set.tsv"
+    dev_path = "twitter_download/dev_set.tsv"
+    test_path = "twitter_download/test_set.tsv"
 
-	train_tweets = load_tsv(train_path)
-	dev_tweets = load_tsv(dev_path)
-	test_tweets = load_tsv(test_path)
+    train_tweets = load_tsv(train_path)
+    dev_tweets = load_tsv(dev_path)
+    test_tweets = load_tsv(test_path)
 
-	print("After removing duplicates and tweets that are the same but have different labels:")
-	print(len(train_tweets),"remained in train set")
-	print(len(dev_tweets),"remained in dev set")
-	print(len(test_tweets),"remained in devtest set")
+    print("After removing duplicates and tweets that are the same but have different labels:")
+    print(len(train_tweets),"remained in train set")
+    print(len(dev_tweets),"remained in dev set")
+    print(len(test_tweets),"remained in devtest set")
 
-	#remove tweets that the text was not available
-	cleaned_train_tweets = clean_tweets(train_tweets)
-	cleaned_dev_tweets = clean_tweets(dev_tweets)
-	cleaned_test_tweets = clean_tweets(test_tweets)
+    #remove tweets that the text was not available
+    cleaned_train_tweets = clean_tweets(train_tweets)
+    cleaned_dev_tweets = clean_tweets(dev_tweets)
+    cleaned_test_tweets = clean_tweets(test_tweets)
 
-	print("Removed", len(train_tweets)-len(cleaned_train_tweets), "\"Not Available\" tweets from train set")
-	print("Removed", len(dev_tweets)-len(cleaned_dev_tweets), "\"Not Available\" tweets from dev set")
-	print("Removed", len(test_tweets)-len(cleaned_test_tweets), "\"Not Available\" tweets from test set")
-
-
-	print("Merging existing sets...")
-
-	print("There are", len(cleaned_train_tweets) + len(cleaned_dev_tweets) + len(cleaned_test_tweets), "in total")
-
-	#merge the sets removing duplicates with different label
-	train_dev = merge(cleaned_train_tweets, cleaned_dev_tweets)
-	total_tweets = merge(train_dev, cleaned_test_tweets)
-
-	print("Merged set has", len(total_tweets), "tweets")
-
-	labels = [total_tweets[tweet][0] for tweet in total_tweets]
-
-	count_labels = Counter(labels)
-
-	print(count_labels)
-
-	#split randomly to train, val, dev and test sets
-	random.seed(42)
-	keys = list(total_tweets.keys())
-	random.shuffle(keys)
-
-	train_split = int(numpy.floor(len(total_tweets) * 0.8))
-
-	train_keys = keys[:train_split]
-	test_keys = keys[train_split:]
-
-	train_tweets = split_data(total_tweets, train_keys)
-	test_tweets = split_data(total_tweets, test_keys)
-
-	#create dataset folder
-	try:
-		rmtree('dataset/')
-	except BaseException:
-		pass  # directory doesn't yet exist, no need to clear it
-	os.makedirs("dataset/")
-
-	#write the resulted sets to tsv files
-	write_tsv(train_tweets, "./dataset/train_set.tsv")
-	write_tsv(test_tweets, "./dataset/test_set.tsv")
-
-	print("Final dataset consists of:")
-	print("Train set:", len(train_tweets))
-	print("Test set:", len(test_tweets))
+    print("Removed", len(train_tweets)-len(cleaned_train_tweets), "\"Not Available\" tweets from train set")
+    print("Removed", len(dev_tweets)-len(cleaned_dev_tweets), "\"Not Available\" tweets from dev set")
+    print("Removed", len(test_tweets)-len(cleaned_test_tweets), "\"Not Available\" tweets from test set")
 
 
+    print("Merging existing sets...")
+
+    print("There are", len(cleaned_train_tweets) + len(cleaned_dev_tweets) + len(cleaned_test_tweets), "in total")
+
+    #merge the sets removing duplicates with different label
+    train_dev = merge(cleaned_train_tweets, cleaned_dev_tweets)
+    total_tweets = merge(train_dev, cleaned_test_tweets)
+
+    print("Merged set has", len(total_tweets), "tweets")
+
+    labels = [total_tweets[tweet][0] for tweet in total_tweets]
+
+    count_labels = Counter(labels)
+
+    print(count_labels)
+
+    #split randomly to train, val, dev and test sets
+    random.seed(42)
+    keys = list(total_tweets.keys())
+    random.shuffle(keys)
+
+    train_split = int(numpy.floor(len(total_tweets) * 0.8))
+
+    train_keys = keys[:train_split]
+    test_keys = keys[train_split:]
+
+    train_tweets = split_data(total_tweets, train_keys)
+    test_tweets = split_data(total_tweets, test_keys)
+
+    #create dataset folder
+    try:
+        rmtree('dataset/')
+    except BaseException:
+        pass  # directory doesn't yet exist, no need to clear it
+    os.makedirs("dataset/")
+
+    pr_train_tweets = process_tweets(train_tweets)
+    pr_test_tweets = process_tweets(test_tweets)
+
+    #write the resulted sets to tsv files
+    write_tsv(pr_train_tweets, "./dataset/train_set.tsv")
+    write_tsv(pr_test_tweets, "./dataset/test_set.tsv")
+
+    print("Final dataset consists of:")
+    print("Train set:", len(pr_train_tweets))
+    print("Test set:", len(pr_test_tweets))
 
 if __name__ == "__main__":
 	main()
